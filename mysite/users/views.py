cdc3c909a23ebe4f3e619e56ab366573630342cc
kijugwak <rhkrrlwn@naver.com>
2022-12-21 15:19:42 +0900

    feat: 로그인 구현

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
 def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            print("인증성공")
+            login(request, user)
+        else:
+            print("인증실패")
     return render(request, "users/login.html")
